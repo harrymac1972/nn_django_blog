97c5a24c720a5fb2beaa61fe971ee45108fa88c1
harry <harrymac1972@gmail.com>
2024-04-04 15:49:57 -0600

    articles app started, registered, urls and views working

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -1,10 +1,12 @@
 
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('articles/',include('articles.urls')),
     path('about/',views.about),
     path('',views.homepage),
 ]
